851ae7a2738f7704be88826097091f6f244388aa
daoodaba975 <davinhoba9@outlook.fr>
2022-03-24 15:37:11 +0000

    ✅🔁

--- app.js
+++ app.js
@@ -3,53 +3,45 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-//import les models
-const db=require("./models")
-//
-
-const indexRouter = require('./routes/index');
-const articlesRouter = require('./routes/articles');
-const blogRouter = require('./routes/blog');
-const contactRouter = require('./routes/contact');
-const detailsArticlesRouter = require('./routes/detail');
-const AproposRouter = require('./routes/apropos');
-
-//syscronise db
-// db.sequelize.sync()
-
-//
 const app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Use Router
+const indexRouter = require('./routes/index');
+const articlesRouter = require('./routes/articles');
+const blogRouter = require('./routes/blog');
+const detailsArticlesRouter = require('./routes/details');
+const aproposRouter = require('./routes/apropos');
+const contactRouter = require('./routes/contact');
+
+// Render page
 app.use('/', indexRouter);
 app.use('/articles', articlesRouter);
 app.use('/blog', blogRouter);
-app.use('/contact', contactRouter);
 app.use('/detailsArticles', detailsArticlesRouter);
-app.use('/Apropos', AproposRouter);
+app.use('/apropos', aproposRouter);
+app.use('/contact', contactRouter);
 
-
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
