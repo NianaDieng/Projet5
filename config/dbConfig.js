ec7016465caf008c7a51b00ea5bc492248db438a
daoodaba975 <davinhoba9@outlook.fr>
2022-08-01 17:15:47 +0000

    fix error ✔️

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,11 @@
-// const express=require('express')
+// ? const express=require('express')
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/blogging",
-{ useNewUrlParser: true,
-  useUnifiedTopology: true })
+
+mongoose.connect("mongodb://localhost:27017/blogging", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    }
+)
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
--- routes/articles.js
+++ routes/articles.js
@@ -1,22 +1,12 @@
-const router = require("express").Router();
 const Articlemodel = require("../config/Articlemodel");
-router
-  .get("/articles", (req, res) => {
-    res.render("composeBlog");
-  })
-  .post("/articles", (req, res) => {
-    const { title, author,resume,contenu } = req.body;
-    // Rechercher les champs manquants
-    if (!title || !author || !resume || !contenu)
-      return res.send("Please enter all the required credentials!");
-    const newBlog = new Articlemodel({ title, author,resume,contenu });
-    // enregister le blog dans la base de donnees
-    newBlog
-      .save()
-      .then(() => {
-        console.log("Blog Saved Successfully!");
-        res.redirect("/");
-      })
-      .catch((err) => console.log(err));
-  });
+
+var express = require('express');
+var router = express.Router();
+
+
+router.
+get('/', function(req, res, next) {
+  res.render('articles', { title: 'Ajouter un article' });
+});
+
 module.exports = router;
